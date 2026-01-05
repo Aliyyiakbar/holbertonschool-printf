ce45dffccf86a5362820349bb2219bd0dcaa2253
Aliyyiakbar <mr.bluetuxedoman@gmail.com>
2026-01-05 17:07:00 +0400

    deleted old files such as get_specifier.c and specifiers.c, updated print_int.c and print_pointer.c to use C90

--- print_int.c
+++ print_int.c
@@ -4,13 +4,12 @@
  * pf_build_digits - build reversed digits of num in base
  * @num: number
  * @base: base
- * @upper: upper hex
+ * @upper: uppercase hex
  * @digits: output reversed digits
  *
- * Return: count
+ * Return: digit count
  */
-static int pf_build_digits(unsigned long long num, int base, int upper,
-			   char *digits)
+static int pf_build_digits(unsigned long num, int base, int upper, char *digits)
 {
 	int i;
 	unsigned int d;
@@ -24,9 +23,9 @@ static int pf_build_digits(unsigned long long num, int base, int upper,
 
 	while (num > 0)
 	{
-		d = (unsigned int)(num % (unsigned long long)base);
+		d = (unsigned int)(num % (unsigned long)base);
 		digits[i++] = pf_hex_digit(d, upper);
-		num /= (unsigned long long)base;
+		num /= (unsigned long)base;
 	}
 	return (i);
 }
@@ -51,71 +50,46 @@ static int pf_print_num_core(pf_buffer_t *b, pf_format_t *f, char *digits,
 
 	zeros = 0;
 	if (f->precision >= 0 && f->precision > dlen)
-	{
 		zeros = f->precision - dlen;
-	}
 
 	total = dlen + zeros + plen + (sign ? 1 : 0);
-	pad = (f->width > total) ? (f->width - total) : 0;
+
+	pad = 0;
+	if (f->width > total)
+		pad = f->width - total;
 
 	padc = ' ';
 	if ((f->flags & PF_F_ZERO) && !(f->flags & PF_F_MINUS) && f->precision < 0)
-	{
 		padc = '0';
-	}
 
 	if (!(f->flags & PF_F_MINUS) && padc == ' ')
-	{
 		if (pf_buf_pad(b, ' ', pad) == -1)
-		{
 			return (-1);
-		}
-	}
 
 	if (sign)
-	{
 		if (pf_buf_putc(b, sign) == -1)
-		{
 			return (-1);
-		}
-	}
 
 	if (plen)
-	{
 		if (pf_buf_putn(b, pref, plen) == -1)
-		{
 			return (-1);
-		}
-	}
 
 	if (!(f->flags & PF_F_MINUS) && padc == '0')
-	{
 		if (pf_buf_pad(b, '0', pad) == -1)
-		{
 			return (-1);
-		}
-	}
 
 	if (pf_buf_pad(b, '0', zeros) == -1)
-	{
 		return (-1);
-	}
 
 	while (dlen > 0)
 	{
 		if (pf_buf_putc(b, digits[--dlen]) == -1)
-		{
 			return (-1);
-		}
 	}
 
 	if (f->flags & PF_F_MINUS)
-	{
 		if (pf_buf_pad(b, ' ', pad) == -1)
-		{
 			return (-1);
-		}
-	}
 
 	return (0);
 }
@@ -130,48 +104,37 @@ static int pf_print_num_core(pf_buffer_t *b, pf_format_t *f, char *digits,
  */
 int pf_print_signed(pf_buffer_t *b, pf_format_t *f, va_list *ap)
 {
-	long long n;
-	unsigned long long num;
+	long n;
+	unsigned long num;
 	char digits[65];
 	int dlen;
 	char sign;
 
 	if (f->length == 'l')
-	{
-		n = (long)va_arg(*ap, long);
-	}
+		n = va_arg(*ap, long);
 	else if (f->length == 'h')
-	{
 		n = (short)va_arg(*ap, int);
-	}
 	else
-	{
-		n = (int)va_arg(*ap, int);
-	}
+		n = va_arg(*ap, int);
 
 	sign = 0;
 	if (n < 0)
 	{
 		sign = '-';
-		num = (unsigned long long)(-n);
+		num = (unsigned long)(-(n + 1));
+		num = num + 1;
 	}
 	else
 	{
 		if (f->flags & PF_F_PLUS)
-		{
 			sign = '+';
-		}
-		else if (f->flags & PF_F_SPACE)
-		{
+		if (sign == 0 && (f->flags & PF_F_SPACE))
 			sign = ' ';
-		}
-		num = (unsigned long long)n;
+		num = (unsigned long)n;
 	}
 
 	if (num == 0 && f->precision == 0)
-	{
 		return (pf_print_num_core(b, f, digits, 0, sign, "", 0));
-	}
 
 	dlen = pf_build_digits(num, 10, 0, digits);
 	return (pf_print_num_core(b, f, digits, dlen, sign, "", 0));
@@ -190,24 +153,18 @@ int pf_print_signed(pf_buffer_t *b, pf_format_t *f, va_list *ap)
 int pf_print_unsigned_base(pf_buffer_t *b, pf_format_t *f, va_list *ap,
 			   int base, int upper)
 {
-	unsigned long long num;
+	unsigned long num;
 	char digits[65];
 	int dlen;
 	const char *pref;
 	int plen;
 
 	if (f->length == 'l')
-	{
-		num = (unsigned long)va_arg(*ap, unsigned long);
-	}
+		num = va_arg(*ap, unsigned long);
 	else if (f->length == 'h')
-	{
 		num = (unsigned short)va_arg(*ap, unsigned int);
-	}
 	else
-	{
-		num = (unsigned int)va_arg(*ap, unsigned int);
-	}
+		num = va_arg(*ap, unsigned int);
 
 	pref = "";
 	plen = 0;
@@ -219,12 +176,12 @@ int pf_print_unsigned_base(pf_buffer_t *b, pf_format_t *f, va_list *ap,
 			pref = "0";
 			plen = 1;
 		}
-		else if (base == 16 && !upper)
+		if (base == 16 && !upper)
 		{
 			pref = "0x";
 			plen = 2;
 		}
-		else if (base == 16 && upper)
+		if (base == 16 && upper)
 		{
 			pref = "0X";
 			plen = 2;
@@ -243,4 +200,27 @@ int pf_print_unsigned_base(pf_buffer_t *b, pf_format_t *f, va_list *ap,
 
 	dlen = pf_build_digits(num, base, upper, digits);
 	return (pf_print_num_core(b, f, digits, dlen, 0, pref, plen));
+}
+
+/**
+ * pf_print_binary - prints %b (unsigned int to binary)
+ * @b: buffer
+ * @f: format
+ * @ap: args
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int pf_print_binary(pf_buffer_t *b, pf_format_t *f, va_list *ap)
+{
+	unsigned long num;
+	char digits[65];
+	int dlen;
+
+	num = (unsigned long)va_arg(*ap, unsigned int);
+
+	if (num == 0 && f->precision == 0)
+		return (pf_print_num_core(b, f, digits, 0, 0, "", 0));
+
+	dlen = pf_build_digits(num, 2, 0, digits);
+	return (pf_print_num_core(b, f, digits, dlen, 0, "", 0));
 }
--- print_pointer.c
+++ print_pointer.c
@@ -1,29 +1,23 @@
 #include "main.h"
 
 /**
- * pf_build_digits_ptr - build reversed hex digits (lowercase)
+ * pf_build_ptr_digits - build reversed lowercase hex digits
  * @num: number
  * @digits: output reversed digits
  *
- * Return: count
+ * Return: digit count
  */
-static int pf_build_digits_ptr(unsigned long long num, char *digits)
+static int pf_build_ptr_digits(unsigned long num, char *digits)
 {
 	int i;
 	unsigned int d;
 
 	i = 0;
-	if (num == 0)
-	{
-		digits[i++] = '0';
-		return (i);
-	}
-
 	while (num > 0)
 	{
-		d = (unsigned int)(num % 16ULL);
+		d = (unsigned int)(num % 16UL);
 		digits[i++] = pf_hex_digit(d, 0);
-		num /= 16ULL;
+		num /= 16UL;
 	}
 	return (i);
 }
@@ -38,77 +32,66 @@ static int pf_build_digits_ptr(unsigned long long num, char *digits)
  */
 int pf_print_pointer(pf_buffer_t *b, pf_format_t *f, va_list *ap)
 {
-	unsigned long long num;
+	unsigned long num;
 	char digits[65];
 	int dlen, pad, total;
+	char padc;
 	const char *nil;
 
 	nil = "(nil)";
-	num = (unsigned long long)(unsigned long)va_arg(*ap, void *);
+	num = (unsigned long)va_arg(*ap, void *);
 
 	if (num == 0)
 	{
-		total = 5;
-		pad = (f->width > total) ? (f->width - total) : 0;
+		pad = 0;
+		if (f->width > 5)
+			pad = f->width - 5;
 
 		if (!(f->flags & PF_F_MINUS))
-		{
 			if (pf_buf_pad(b, ' ', pad) == -1)
-			{
 				return (-1);
-			}
-		}
 
 		if (pf_buf_putn(b, nil, 5) == -1)
-		{
 			return (-1);
-		}
 
 		if (f->flags & PF_F_MINUS)
-		{
 			if (pf_buf_pad(b, ' ', pad) == -1)
-			{
 				return (-1);
-			}
-		}
 
 		return (0);
 	}
 
-	dlen = pf_build_digits_ptr(num, digits);
+	dlen = pf_build_ptr_digits(num, digits);
 
-	/* prefix "0x" always, width applies, zero flag allowed unless '-' */
-	total = 2 + dlen;
-	pad = (f->width > total) ? (f->width - total) : 0;
+	total = 2 + dlen; /* "0x" + digits */
+	pad = 0;
+	if (f->width > total)
+		pad = f->width - total;
 
-	if (!(f->flags & PF_F_MINUS))
-	{
+	padc = ' ';
+	if ((f->flags & PF_F_ZERO) && !(f->flags & PF_F_MINUS))
+		padc = '0';
+
+	if (!(f->flags & PF_F_MINUS) && padc == ' ')
 		if (pf_buf_pad(b, ' ', pad) == -1)
-		{
 			return (-1);
-		}
-	}
 
 	if (pf_buf_putn(b, "0x", 2) == -1)
-	{
 		return (-1);
-	}
+
+	if (!(f->flags & PF_F_MINUS) && padc == '0')
+		if (pf_buf_pad(b, '0', pad) == -1)
+			return (-1);
 
 	while (dlen > 0)
 	{
 		if (pf_buf_putc(b, digits[--dlen]) == -1)
-		{
 			return (-1);
-		}
 	}
 
 	if (f->flags & PF_F_MINUS)
-	{
 		if (pf_buf_pad(b, ' ', pad) == -1)
-		{
 			return (-1);
-		}
-	}
 
 	return (0);
 }
